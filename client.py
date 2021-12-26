ebf6b1ec26450c58ca126a52bfb3f4183bc4112e
Nivfono2001 <81295283+Nivfono2001@users.noreply.github.com>
2021-12-26 23:40:59 +0200

    with two clients limitation

--- client.py
+++ client.py
@@ -1,10 +1,34 @@
 import socket
+import struct
+import time
+import binascii
+
+
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 13117
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+print("here"+str(1))
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)#defining broadcast
+print("here"+str(2))
+
+sock.bind(('', MCAST_PORT))
+print("here"+str(3))
+
+mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+print("here"+str(4))
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+print("here"+str(5))
+TCP_PORT=struct.unpack('LBh',sock.recv(1024))[2]
+print("CONNECTING TO"+str(TCP_PORT))
 ip=socket.gethostbyname(socket.gethostname())
-port=1234
+port=TCP_PORT
 server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.connect((ip,port))
 print("Game Started!")
 isTrue=True
+myname="UNICORNS/TCPICKACHU(SHEM ZMANI)\n"
+server.send(bytes(myname,"utf-8"))
 while(isTrue):
     question=server.recv(2048)
     question=question.decode("utf-8")
@@ -16,4 +40,30 @@ while(isTrue):
     print(isTrue)
 total_score=server.recv(2048)
 total_score=total_score.decode("utf-8")
-print(question)    
+print(question)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+# UDP_IP = "127.0.0.1"
+# UDP_PORT = 13117
+# while True: 
+#     sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM) # UDP
+#     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+#     sock.bind(('', UDP_PORT))#RECIEVING FROM ALL INTERFACES
+#     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+#     data=int(data)
+#     print("port is: %s" % data)
+#     time.sleep(2)
+    
+
+  
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import random
 import threading
 import time
+import struct
 FORMAT="utf-8"
 #return a random question and its answer
 def QuestionGenerator():
@@ -10,19 +11,14 @@ def QuestionGenerator():
      n2=random.randint(-20,20)
      return (n1+n2,str(n1)+"+"+str(n2)+"?")   
 
-#def countdown(t):
-#    while t:
-#        mins, secs = divmod(t, 60)
-#        timer = '{:02d}:{:02d}'.format(mins, secs)
-#        print(timer, end="\r")
-#        time.sleep(1)
-#        t -= 1
-         
+
 def handleClient(client,adress):
      true_answer,ans_from_client="",""
      ctr=0     
+     print(thread.getName())
+     name_of_client=client.recv(2048)
+     print("welcome:"+str(name_of_client.decode("utf-8")))
      while(str(true_answer)==ans_from_client):  
-          print(thread.getName())
           true_answer,question=QuestionGenerator()
           client.send(bytes(question,"utf-8"))
           ans_from_client=client.recv(2048)
@@ -34,19 +30,100 @@ def handleClient(client,adress):
      ctr-=1          
      client.send(bytes("total score:"+str(ctr),FORMAT))     
      client.close()
+def Broadcasting():
+     group = '224.1.1.1'
+     port = 1234
+     # 2-hop restriction in network
+     ttl = 2
+     sock = socket.socket(socket.AF_INET,
+                     socket.SOCK_DGRAM,
+                     socket.IPPROTO_UDP)
+     sock.setsockopt(socket.IPPROTO_IP,
+                socket.IP_MULTICAST_TTL,
+                ttl)
 
-  
+     udp_packet=struct.pack('LBh',0xabcddcba,0x2,port)    
+            
+     while STOP_BROADCAST:
+          sock.sendto(udp_packet, (group, 13117))
+          time.sleep(1)
+          print("sending again")   
+global STOP_BROADCAST
+STOP_BROADCAST=True
+TCP_PORT=1234
+MYPORT = 13117
+udp_thread=threading.Thread(target=Broadcasting)
+udp_thread.start()
 ip=socket.gethostbyname(socket.gethostname())
 port=1234
 server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((ip,port))
 server.listen(2)#number of clients?
-while(True):
+max_Client=2
+connected_Clients=0
+while connected_Clients<max_Client:
      client, adress=server.accept()
+     connected_Clients+=1
      thread=threading.Thread(target=handleClient, args=(client,adress))
-     print("connection established!")
+     print("connection established: number of clients:"+str(connected_Clients))
      thread.start()
-     thread.join()     
+STOP_BROADCAST=False
+  
+ 
+     
+
+
+
+
+
+
+
+# def Broadcasting():
+#      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#      s.bind(('', 5555))
+#      s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+#      while 1:
+#           # first_part=0xabcddcba+0x2
+#           data =TCP_PORT
+#           s.sendto(bytes(str(data),FORMAT), ('255.255.255.255', MYPORT))
+#           time.sleep(2)
+
+
+# TCP_PORT=1234
+# MYPORT = 13117
+# udp_thread=threading.Thread(target=Broadcasting)
+# udp_thread.start()
+# ip=socket.gethostbyname(socket.gethostname())
+# port=1234
+# server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.bind((ip,port))
+# server.listen(2)#number of clients?
+# ctr=0
+# while(True):
+#      if ctr<2:
+#           client, adress=server.accept()
+#           ctr+=1
+#      #thread=threading.Thread(target=handleClient, args=(client,adress))
+#      print("connection established!")
+
 
 
      
+
+
+
+
+
+# UDP_IP = "127.0.0.1"
+# UDP_PORT = 13117
+# MESSAGE = b"Please Enter Our Game!!!!"
+ 
+# print("UDP target IP: %s" % UDP_IP)
+# print("UDP target port: %s" % UDP_PORT)
+# print("message: %s" % MESSAGE)
+
+#      sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM) # UDP
+#      sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))  
+#      time.sleep(2)
+#      print(i)
+
