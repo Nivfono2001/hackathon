90c15152ef2d2993a85e559b9cd9505f6fd62eee
Nivfono2001 <81295283+Nivfono2001@users.noreply.github.com>
2021-12-29 12:02:28 +0200

    with valid struct

--- client.py
+++ client.py
@@ -15,29 +15,36 @@ while(True):
     print("Recieved offer from "+ str(server_address[0])+" , attempting to connect...")
 
     #connetion to server in TCP protocol
-    TCP_PORT=struct.unpack('IbH', first_pack)[2]
-    ip=socket.gethostbyname(socket.gethostname())
-    port=TCP_PORT
-    server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.connect((ip, port))
-    isTrue=True
-    myname="UNICORNS\n"
-    id=input("enter id")
-    print("The winner team AKA the Unicorns client started, waiting for your offers...")
-    server.send(bytes(myname+str(id),"utf-8"))#sending name
     try:
-        #playing the game
-        question=server.recv(2048)
-        question=question.decode("utf-8")
-        print(question)#recieving question
-        answer=input("Enter answer:")
-        server.send(bytes(answer,"utf-8"))
-        results = server.recv(2048)
-        results = results.decode("utf-8")
-        print(results)  
+        pack=struct.unpack('IbH', first_pack)
+        TCP_PORT=pack[2]
+        if(pack!=int(0xabcddcba)):
+            raise "invalid first num"
+        if(pack!=int(0x2)):  
+                raise "invalid second num"
+        ip=socket.gethostbyname(socket.gethostname())
+        port=TCP_PORT
+        server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.connect((ip, port))
+        isTrue=True
+        myname="UNICORNS\n"
+        id=input("enter id")
+        print("The winner team AKA the Unicorns client started, waiting for your offers...")
+        server.send(bytes(myname+str(id),"utf-8"))#sending name
+        try:
+            #playing the game
+            question=server.recv(2048)
+            question=question.decode("utf-8")
+            print(question)#recieving question
+            answer=input("Enter answer:")
+            server.send(bytes(answer,"utf-8"))
+            results = server.recv(2048)
+            results = results.decode("utf-8")
+            print(results)  
+        except:
+            print("disconnected from server")
     except:
-        print("disconnected from server")
-    
-
+        print("could not enter this server")        
+        print(struct.calcsize('IbH'))
 
     
